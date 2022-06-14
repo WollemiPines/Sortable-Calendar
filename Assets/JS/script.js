2ab8a4fc31d1857b63156b42d0451d60feeecd0f
Chris Wallace <Chrisw1096@gmail.com>
2022-06-14 16:09:26 +1000

    cleaned redundant JS scritp

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,87 +10,86 @@ function disp_day(){
 disp_day();
 
 
-let current_time;
 
+function createFormEL(){
+    $("ul li.ui-state-default").html('<form id="form-0" name="formElParent" onSubmit="return false"><input type="text" name="formEl" value="4"></form>');
+    $("input").addClass("form-El-Class");
+    
+};
+
+$(".saveBtn").click(saveBtn);
+
+function saveBtn(){
+        // Get all the forms elements and their values in one step
+        var values = ($('#form-0'));
+        localStorage.setItem("userInput", values);
+        console.log(values);
+}
+
+
+let get = localStorage.getItem("userInput")
+console.log(get);
+
+
+createFormEL()
+
+
+let current_time;
 var before = $("ul li.ui-state-default:lt(0)");
 var current = $("ul li.ui-state-default:nth-child(1)");
 var after = $("ul li.ui-state-default:gt(0)");
 
-
 function calc_current_time(){
   current_time = moment().format("ha");
 
-   if(current_time=="9am"){
-   current.addClass('present');
-   after.addClass('future');
-   }
    if(current_time=="10am"){
      before = $("ul li.ui-state-default:lt(1)");
     current = $("ul li.ui-state-default:nth-child(2)");
      after = $("ul li.ui-state-default:gt(1)");
-     before.addClass('past');
-    current.addClass('present');
-    after.addClass('future');
     }
     if(current_time=="11am"){
         before = $("ul li.ui-state-default:lt(2)");
        current = $("ul li.ui-state-default:nth-child(3)");
         after = $("ul li.ui-state-default:gt(2)");
-        before.addClass('past');
-       current.addClass('present');
-       after.addClass('future');
        }
        if(current_time=="12pm"){
         before = $("ul li.ui-state-default:lt(3)");
        current = $("ul li.ui-state-default:nth-child(4)");
         after = $("ul li.ui-state-default:gt(3)");
-        before.addClass('past');
-       current.addClass('present');
-       after.addClass('future');
        }
        if(current_time=="1pm"){
         before = $("ul li.ui-state-default:lt(4)");
        current = $("ul li.ui-state-default:nth-child(5)");
         after = $("ul li.ui-state-default:gt(4)");
-        before.addClass('past');
-       current.addClass('present');
-       after.addClass('future');
        }
        if(current_time=="2pm"){
         before = $("ul li.ui-state-default:lt(5)");
        current = $("ul li.ui-state-default:nth-child(6)");
         after = $("ul li.ui-state-default:gt(5)");
-        before.addClass('past');
-       current.addClass('present');
-       after.addClass('future');
        }
        if(current_time=="3pm"){
         before = $("ul li.ui-state-default:lt(6)");
        current = $("ul li.ui-state-default:nth-child(7)");
         after = $("ul li.ui-state-default:gt(6)");
-        before.addClass('past');
-       current.addClass('present');
-       after.addClass('future');
        }
        if(current_time=="4pm"){
         before = $("ul li.ui-state-default:lt(7)");
        current = $("ul li.ui-state-default:nth-child(8)");
         after = $("ul li.ui-state-default:gt(7)");
-        before.addClass('past');
-       current.addClass('present');
-       after.addClass('future');
        }
        if(current_time=="5pm"){
         before = $("ul li.ui-state-default:lt(8)");
        current = $("ul li.ui-state-default:nth-child(9)");
         after = $("ul li.ui-state-default:gt(8)");
-        before.addClass('past');
+       }
+
+       before.addClass('past');
        current.addClass('present');
        after.addClass('future');
-       }
 }
 
 calc_current_time();
+
 
 
 console.log(current_time);
@@ -102,3 +101,8 @@ $( function() {
     });
     $( ".time-block" ).disableSelection();
   } );
+
+  calc_current_time();
+
+
+//   <input type="text" name="add_name"></input>
